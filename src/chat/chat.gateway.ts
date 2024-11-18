da010efa497e723d31e351e1ea9d73b1a6313677
KNU-K <knukang334@gmail.com>
2024-11-18 23:05:19 +0900

    feat(chat) : initialize chat gateway as  websocket

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,16 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({})
+export class ChatGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(client: any) {
+    client.emit('message', 'Hello world!');
+  }
+}
