17664a41d48e6436a921d5203dda5133ca16b722
KNU-K <knukang334@gmail.com>
2024-11-18 23:04:41 +0900

    feat : add CustomIOAdapter
    - this adapter have fix url about ws

---
+++ src/common/adapter/custom-io-adapter.ts
@@ -0,0 +1,15 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Server } from 'socket.io';
+
+export class CustomIoAdapter extends IoAdapter {
+  createIOServer(port: number, options?: any): Server {
+    try {
+      const server = super.createIOServer(port, options);
+      server.of('/ws');
+      return server;
+    } catch (error) {
+      console.error('Error creating Socket.IO server:', error);
+      throw new Error('Failed to create Socket.IO server');
+    }
+  }
+}
