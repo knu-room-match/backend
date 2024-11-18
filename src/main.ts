6fe6891bd513ca3a6c61f54dbdd7c2012dcd5bff
KNU-K <knukang334@gmail.com>
2024-11-18 23:06:10 +0900

    feat(app) : apply the customioadapter in app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { CustomIoAdapter } from './common/adapter/custom-io-adapter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const customIoAdapter = new CustomIoAdapter(app);
+  app.useWebSocketAdapter(customIoAdapter);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
