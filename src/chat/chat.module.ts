c2f283e855260d124a8860dda36042e48033b66f
KNU-K <knukang334@gmail.com>
2024-11-19 15:57:52 +0900

    config : register schema in module(chat, survey)

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from './schemas/message.schema';
 
 @Module({
-  providers: [ChatGateway],
+  imports: [
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
+  providers: [ChatGateway, ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
