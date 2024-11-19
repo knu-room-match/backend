722981f4c4c3a59763b20fa689a435547bdcd22f
KNU-K <knukang334@gmail.com>
2024-11-19 21:46:26 +0900

    feat(user) : add user entity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
