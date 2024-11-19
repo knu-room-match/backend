cc47f931d55221dc4cf1b5770008fff15bc45018
KNU-K <knukang334@gmail.com>
2024-11-19 15:56:37 +0900

    config : inject config about mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+import { SurveyModule } from './survey/survey.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/your-database-name'),
+    ChatModule,
+    UserModule,
+    SurveyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
