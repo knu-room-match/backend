ef2cb7fd8c8aea802e6fab7cd7fff9ba0a82a62a
KNU-K <knukang334@gmail.com>
2024-11-20 02:01:34 +0900

    config(app) : import UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
 import { SurveyModule } from './survey/survey.module';
 import { DatabaseModule } from './common/database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ChatModule, SurveyModule, DatabaseModule],
+  imports: [DatabaseModule, ChatModule, SurveyModule, UserModule],
 })
 export class AppModule {}
