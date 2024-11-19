31a86a0026a39b89b2faa0fd0c5bba2fca31990a
KNU-K <knukang334@gmail.com>
2024-11-19 20:59:17 +0900

    config : simplify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { SurveyModule } from './survey/survey.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/your-database-name'),
-    ChatModule,
-    SurveyModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ChatModule, SurveyModule, DatabaseModule],
 })
 export class AppModule {}
