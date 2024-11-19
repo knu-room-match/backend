f1df7a04f7d35ff67bb3c57505602b6e7c93e2a5
KNU-K <knukang334@gmail.com>
2024-11-20 04:48:03 +0900

    config : add mongoConfiguration

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateway/chat.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Message, MessageSchema } from './schemas/message.schema';
+import { ChatController } from './controller/chat.controller';
+import { ChatService } from './service/chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chatroom } from './entities/chatroom.entity';
+import { ChatParticipants } from './entities/chat-participants.entity';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }])],
-  providers: [ChatGateway],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([Chatroom, ChatParticipants]),
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
+  providers: [ChatGateway, ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
