cf009ddf2e32d11be1db0b770853cb722027b1b1
KNU-K <knukang334@gmail.com>
2024-11-20 02:01:06 +0900

    config(user) : config TypeOrmModule in User

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
+  exports: [TypeOrmModule],
 })
 export class UserModule {}
