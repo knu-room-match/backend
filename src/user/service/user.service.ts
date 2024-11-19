7803312996d52cfd2ec2e5619f488778da0dc64e
KNU-K <knukang334@gmail.com>
2024-11-20 01:59:50 +0900

    feat(user) : add service in user domain

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDTO } from '../dto/user-request.dto';
 
 @Injectable()
 export class UserService {
@@ -9,4 +10,14 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+  async create(createUserDTO: CreateUserDTO): Promise<User> {
+    const user = this.userRepository.create(createUserDTO);
+    return await this.userRepository.save(user);
+  }
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+  async findById(id: number): Promise<User> {
+    return await this.userRepository.findOneBy({ id });
+  }
 }
