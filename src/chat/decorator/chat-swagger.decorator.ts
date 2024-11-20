e959557c34747bcc9183b813551028298a199d91
KNU-K <knukang334@gmail.com>
2024-11-20 15:39:21 +0900

    refactor : clean up swagger notation using custom decorator

--- src/chat/decorator/chat-swagger.decorator.ts
+++ src/chat/decorator/chat-swagger.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { CreateChatDTO } from '../dto/chat-request';
 
 export class ChatSwaggerDocs {
@@ -24,6 +24,13 @@ export class ChatSwaggerDocs {
       ApiOperation({ summary: '채팅방 나가기', description: '사용자가 채팅방에서 나갑니다.' }),
       ApiResponse({ status: 200, description: '채팅방 나가기가 완료되었습니다.' }),
       ApiBody({ schema: { example: { userId: 1, roomId: 2 } } }),
+    );
+  }
+  static getMessages() {
+    return applyDecorators(
+      ApiOperation({ summary: '채팅방 메시지 조회', description: '특정 채팅방의 메시지를 조회합니다.' }),
+      ApiResponse({ status: 200, description: '메시지 조회 성공.' }),
+      ApiParam({ name: 'roomId', description: '채팅방 ID', example: 2 }),
     );
   }
 }
