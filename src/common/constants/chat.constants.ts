27a70b4e7d74c96c30e72747963b59ebf0e2b720
KNU-K <knukang334@gmail.com>
2024-11-21 13:38:37 +0900

    feat(chat) : add constants message for abstracting notation

---
+++ src/common/constants/chat.constants.ts
@@ -0,0 +1,14 @@
+export const CHAT_MESSAGES = {
+  SUCCESS: {
+    CHATROOM_CREATED: '채팅방이 성공적으로 생성되었습니다.',
+    CHATROOM_FOUND: '채팅방이 성공적으로 조회되었습니다.',
+    CHATROOM_ENTERED: '채팅방에 성공적으로 입장했습니다.',
+    CHATROOM_EXITED: '채팅방에서 성공적으로 나갔습니다.',
+    MESSAGES_RETRIEVED: '메시지 조회가 성공적으로 이루어졌습니다.',
+  },
+  ERROR: {
+    CHATROOM_NOT_FOUND: '채팅방을 찾을 수 없습니다.',
+    CHATROOM_CREATION_FAILED: '채팅방 생성에 실패했습니다.',
+    MESSAGE_RETRIEVAL_FAILED: '메시지 조회에 실패했습니다.',
+  },
+};
