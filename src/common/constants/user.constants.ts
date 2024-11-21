100fd36f2875975aa035ed0ad2f0e76fd34196f3
KNU-K <knukang334@gmail.com>
2024-11-21 13:33:46 +0900

    feat(user) : add constant that have user error message

---
+++ src/common/constants/user.constants.ts
@@ -0,0 +1,11 @@
+export const USER_MESSAGES = {
+  SUCCESS: {
+    USER_CREATED: '사용자가 성공적으로 생성되었습니다.',
+    USER_FOUND: '사용자가 성공적으로 조회되었습니다.',
+    USERS_FOUND: '사용자 목록이 성공적으로 조회되었습니다.',
+  },
+  ERROR: {
+    USER_NOT_FOUND: '사용자를 찾을 수 없습니다.',
+    USER_CREATION_FAILED: '사용자 생성에 실패했습니다.',
+  },
+};
