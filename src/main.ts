f6ce2bfadde4b62edccce7d3510ad29a872117b7
KNU-K <knukang334@gmail.com>
2024-11-21 13:40:08 +0900

    refactor(main) : apply globalExceptionFilter in this app

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomIoAdapter } from './common/adapter/custom-io-adapter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './common/filter/global-exception.filter';
+// import { GlobalResponseInterceptor } from './common/interceptor/global-response.interceptor';
+
+const config = new DocumentBuilder()
+  .setTitle('Chat API')
+  .setDescription('Chat service API documentation')
+  .setVersion('1.0')
+  .addTag('chat')
+  .build();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const customIoAdapter = new CustomIoAdapter(app);
+  const globalExceptionFilter = new GlobalExceptionFilter();
+  // const globalResponseInterceptor = new GlobalResponseInterceptor();
+
   app.useWebSocketAdapter(customIoAdapter);
-  const config = new DocumentBuilder()
-    .setTitle('Chat API')
-    .setDescription('Chat service API documentation')
-    .setVersion('1.0')
-    .addTag('chat')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  app.useGlobalFilters(globalExceptionFilter);
+  // app.useGlobalInterceptors(globalResponseInterceptor);
+
+  SwaggerModule.setup('api-docs', app, SwaggerModule.createDocument(app, config));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
