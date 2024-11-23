55b70fd787adb42dca39fa8227319d6c1ace3eb5
KNU-K <knukang334@gmail.com>
2024-11-23 20:25:21 +0900

    style(app) : change prettier and import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,23 @@ import { ChatModule } from './chat/chat.module';
 import { SurveyModule } from './survey/survey.module';
 import { DatabaseModule } from './common/database/database.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { EmailService } from './email/email.service';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [DatabaseModule, ChatModule, SurveyModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.local',
+    }),
+    DatabaseModule,
+    ChatModule,
+    SurveyModule,
+    UserModule,
+    AuthModule,
+    EmailModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { CustomIoAdapter } from './common/adapter/custom-io-adapter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GlobalExceptionFilter } from './common/filter/global-exception.filter';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 // import { GlobalResponseInterceptor } from './common/interceptor/global-response.interceptor';
 
 const config = new DocumentBuilder()
@@ -12,17 +13,28 @@ const config = new DocumentBuilder()
   .addTag('chat')
   .build();
 
+const loadCors = async (app: INestApplication) => {
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+  });
+};
+
+const loadSwagger = async (app: INestApplication) => {
+  SwaggerModule.setup('api-docs', app, SwaggerModule.createDocument(app, config));
+};
+
 async function bootstrap() {
+  console.log(process.env.REFRESH_ALGORITHM);
   const app = await NestFactory.create(AppModule);
 
   const customIoAdapter = new CustomIoAdapter(app);
   const globalExceptionFilter = new GlobalExceptionFilter();
-
+  // const validationPipe = new ValidationPipe();
   app.useWebSocketAdapter(customIoAdapter);
+  // app.useGlobalPipes(validationPipe);
   app.useGlobalFilters(globalExceptionFilter);
-
-  SwaggerModule.setup('api-docs', app, SwaggerModule.createDocument(app, config));
-
+  await loadSwagger(app);
+  await loadCors(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
