dae222dea08dbf9a7e9602a24bcf0afa6979ad46
KNU-K <knukang334@gmail.com>
2024-11-25 17:02:09 +0900

    refactor(shared) : add redis service

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { Global, Module } from '@nestjs/common';
 import { MysqlService } from '@shared/application/database/mysql.service';
 import { MongoService } from '@shared/application/database/mongo.service';
 import { DatabaseModule } from '@shared/infrastructure/database/database.module';
+import { RedisService } from '@shared/application/redis/redis.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [DatabaseModule],
-  providers: [MysqlService, MongoService],
-  exports: [MysqlService, MongoService],
+  imports: [DatabaseModule, ConfigModule],
+  providers: [MysqlService, MongoService, RedisService],
+  exports: [MysqlService, MongoService, RedisService],
 })
 export class SharedModule {}
