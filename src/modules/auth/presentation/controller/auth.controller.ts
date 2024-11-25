d6a8152470ffcfd80352e73e1be0ca23ca80aebf
KNU-K <knukang334@gmail.com>
2024-11-25 09:42:10 +0900

    feat(auth) : add controller about login

---
+++ src/modules/auth/presentation/controller/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from '@auth/application/service/auth.service';
+import { AuthCredentials } from '@auth/application/dto/auth-request.dto';
+import { ResponseEntity } from '@common/dto/response-entity.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  async signIn(@Body() authCredentials: AuthCredentials) {
+    const authResponse = await this.authService.signIn(authCredentials);
+    return ResponseEntity.success(authResponse, '로그인에 성공');
+  }
+}
