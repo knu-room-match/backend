2a812af6883b06f660fb45c75776c05c9eb1db05
KNU-K <knukang334@gmail.com>
2024-11-25 16:07:13 +0900

    feat(auth) : add controller

--- src/modules/auth/presentation/controller/auth.controller.ts
+++ src/modules/auth/presentation/controller/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from '@auth/application/service/auth.service';
 import { AuthCredentials } from '@auth/application/dto/auth-request.dto';
 import { ResponseEntity } from '@common/dto/response-entity.dto';
+import { AuthGuard } from '../guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   async signIn(@Body() authCredentials: AuthCredentials) {
     const authResponse = await this.authService.signIn(authCredentials);
     return ResponseEntity.success(authResponse, '로그인에 성공');
   }
+
+  // @Get('test')
+  // @UseGuards(AuthGuard)
+  // async test(@Request() req) {
+  //   const userId = req.user.id; // 요청 객체에서 userId 접근
+  //   return `User ID: ${userId}`;
+  // }
 }
