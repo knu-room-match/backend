fc5e306d5f31efa602c417dbd176b864b167c433
KNU-K <knukang334@gmail.com>
2024-11-25 09:42:52 +0900

    config(app) : update dependency about auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from '@chat/chat.module';
 import { SurveyModule } from '@survey/survey.module';
-// import { EmailModule } from '@email/email.module';
 import { UserModule } from '@user/user.module';
-// import { DatabaseModule } from '@common/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './modules/shared/shared.module';
-// import { AuthModule } from '@auth/auth.module';
+import { AuthModule } from '@auth/auth.module';
 
 @Module({
   imports: [
@@ -18,7 +16,7 @@ import { SharedModule } from './modules/shared/shared.module';
     SurveyModule,
     UserModule,
     SharedModule,
-    // AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
